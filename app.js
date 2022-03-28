4a7fa0683da566acefc83fc4dbcc2ffabf800d5d
Ege <egeikibin@gmail.com>
2022-03-29 02:46:02 +0300

    the sidebar is now dynamic, also many responsivityfixes

--- app.js
+++ app.js
@@ -14,3 +14,39 @@ var flkty = new Flickity( products, {
   resize: true
 });
 
+//==================PROGRAM=======================//
+
+//PSUEDO CODE 
+// 1- ürün kategorilerini al, sekmelere ekle
+// 2- Sekmelere tıklayınca o kategoriye ait ürünleri/datalarını ve sayılarını edin
+// 3- Ürün sayısını al, sayı kadar ürün kartı oluştur
+// 4- Ürün resimlerini al loop ile her karta ekle 
+// 5- Ürün isimlerini loop ile ekle 
+// 6- Ürün fiyatlarını loop ile ekle
+// 7- bedava kargo true ise bedava kargo kısmını ekle
+
+const categories = document.getElementById("categories");
+
+ async function program() {
+  let response = await fetch("./product-list.json");
+  let productList = await response.json();
+  console.log(productList);
+  console.log(productList.responses[0][0].params.userCategories); 
+
+  // Creating the Sidebar
+  const createSidebar = () => {
+    const userCategories = productList.responses[0][0].params.userCategories; // creating userCategories Array
+    const listLength = Object.keys(userCategories).length // Sidebar items length
+    console.log(listLength);
+    for (i=0; i < listLength; i++ ) {
+        let categoryItem = document.createElement( "div" );
+        categoryItem.classList.add("category-item");
+        categoryItem.appendChild(document.createTextNode(userCategories[i]));
+        categories.appendChild(categoryItem);
+    }
+  }
+
+  createSidebar();
+}
+
+program();
